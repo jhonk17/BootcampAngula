6d5955b2421e8a2f6c60077f9eda11143b16467b
jhonSantiago <jhon.santiagok17@gmail.com>
2023-11-23 23:44:56 -0300

    atualização aula JS

--- JavaScript/04 - Objetos/aula.js
+++ JavaScript/04 - Objetos/aula.js
@@ -1,12 +1,31 @@
-const pessoa = {
-    nome: "Jhon Santiago",
-    idade: 26,
+class pessoa {
 
-    descrever: function (){
+    nome;
+    idade;
+    anoDeNascimento
+
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2022 - idade;
+    }
+
+    descrever() {
         console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
     }
-};
+}
 
-const atributo = 'idade';
+function compararPessoas(p1, p2){
+    if (p1.idade > p2.idade) {
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`);
+    } else if (p2.idade > p1.idade){
+        console.log(`${p2.nome} é mais velho(a) que ${p1.nome}`);
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`)
+    }
+}
 
-console.log(pessoa['nome']);
+const Goku = new pessoa('Goku', 27);
+const Vegeta = new pessoa('Vegeta', 26)
+
+compararPessoas(Goku,Vegeta);
