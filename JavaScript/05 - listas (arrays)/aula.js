918c0e1238d092290ea387df55e1d74fd6e2fde7
jhonSantiago <jhon.santiagok17@gmail.com>
2023-11-27 22:06:32 -0300

    atualizando aula

--- JavaScript/05 - listas (arrays)/aula.js
+++ JavaScript/05 - listas (arrays)/aula.js
@@ -1,13 +1,16 @@
+
 const notas = [];
-notas.push(5);
-notas.push(7);
-notas.push(8);
-notas.push(2);
+
+notas.push(10);
+notas.push(10);
 notas.push(5);
 
-for (let i = 0; i < 10; i ++) {
-   
-    
+
+let soma = 0 
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota   
 }
-
-console.log(notas.length);
+const media = soma / notas.length;
+console.log(media.toFixed(2));
